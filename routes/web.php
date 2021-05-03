e7caf33fcca70e12aa9f34b80637a022f376111d
alihamzehei <alihamzehei2017@gmail.com>
2021-05-03 11:32:47 +0430

    set verify email in auth

--- routes/web.php
+++ routes/web.php
@@ -27,6 +27,6 @@ Route::get('verification-code', function () {
 //
 Route::get('/dashboard', function () {
     return view('dashboard');
-})->middleware(['auth'])->name('dashboard');
+})->middleware(['auth','verified'])->name('dashboard');
 
 require __DIR__ . '/auth.php';
