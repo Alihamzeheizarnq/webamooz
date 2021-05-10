76744325cc0d6c1726e6f00959f5e5f6f094d741
alihamzehei <alihamzehei2017@gmail.com>
2021-05-10 07:23:18 -0700

    update test app

---
+++ modules/User/Test/Feature/LoginUserTest.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Modules\User\Models\User;
+use Tests\TestCase;
+
+class LoginUserTest extends TestCase
+{
+    use RefreshDatabase, WithFaker;
+
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function test_can_user_login_page()
+    {
+        $response = $this->get(route('login'));
+        $response->assertStatus(200);
+    }
+
+    public function test_user_login_by_email()
+    {
+        $user = User::create([
+            'name' => $this->faker->name,
+            'email' => 'alihamzehei@gmail.com',
+            'password' => bcrypt('123456789')
+        ]);
+        $response = $this->post(route('login') , [
+           'email' => 'alihamzehei@gmail.com',
+           'password' => '123456789'
+        ]);
+        $responses = $this->get(route('dashboard'));
+
+        dd($response->assertRedirect(route('verification.notice')));
+        $this->assertAuthenticated();
+
+
+
+    }
+}
