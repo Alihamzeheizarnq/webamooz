4e847378c47db426d9dc9d31134a3342b1738643
alihamzehei <alihamzehei2017@gmail.com>
2021-06-12 13:41:50 +0430

    set test database

--- modules/User/Test/Feature/LoginUserTest.php
+++ modules/User/Test/Feature/LoginUserTest.php
@@ -1,44 +1,43 @@
 <?php
 
-namespace Tests\Feature;
+    namespace Tests\Feature;
 
-use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
-use Modules\User\Models\User;
-use Tests\TestCase;
+    use Illuminate\Foundation\Testing\RefreshDatabase;
+    use Illuminate\Foundation\Testing\WithFaker;
+    use Modules\User\Models\User;
+    use Tests\TestCase;
 
-class LoginUserTest extends TestCase
-{
-    use RefreshDatabase, WithFaker;
-
-    /**
-     * A basic feature test example.
-     *
-     * @return void
-     */
-    public function test_can_user_login_page()
+    class LoginUserTest extends TestCase
     {
-        $response = $this->get(route('login'));
-        $response->assertStatus(200);
+        use RefreshDatabase, WithFaker;
+
+        /**
+         * A basic feature test example.
+         *
+         * @return void
+         */
+        public function test_can_user_login_page()
+        {
+            $response = $this->get(route('login'));
+            $response->assertStatus(200);
+        }
+
+        public function test_user_login_by_email()
+        {
+            User::create([
+                'name' => $this->faker->name,
+                'email' => 'alihamzehei@gmail.com',
+                'password' => bcrypt('123456789')
+            ]);
+            $this->post(route('login'), [
+                'email' => 'alihamzehei@gmail.com',
+                'password' => '123456789'
+            ]);
+            $responses = $this->get(route('dashboard'));
+
+            $responses->assertRedirect(route('verification.notice'));
+            $this->assertAuthenticated();
+
+
+        }
     }
-
-    public function test_user_login_by_email()
-    {
-        $user = User::create([
-            'name' => $this->faker->name,
-            'email' => 'alihamzehei@gmail.com',
-            'password' => bcrypt('123456789')
-        ]);
-        $response = $this->post(route('login') , [
-           'email' => 'alihamzehei@gmail.com',
-           'password' => '123456789'
-        ]);
-        $responses = $this->get(route('dashboard'));
-
-        dd($response->assertRedirect(route('verification.notice')));
-        $this->assertAuthenticated();
-
-
-
-    }
-}
